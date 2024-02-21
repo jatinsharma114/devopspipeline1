461342693639ef9718101f932f6e3254fbe6c13d
jatinsharma114 <jatin2010sharma@gmail.com>
2024-02-22 01:01:51 +0530

    first commit

---
+++ src/main/java/com/springapp/applicationdocker/ApplicationdockerApplication.java
@@ -0,0 +1,13 @@
+package com.springapp.applicationdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationdockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationdockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapp/applicationdocker/ApplicationdockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapp.applicationdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationdockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
